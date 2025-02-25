a1396e63f870ee1245d5eef247dd00692bb8f96b
EliasAarekol <eliasaarekol@gmail.com>
2025-02-25 10:13:38 +0100

    added main loop training skeleton

---
+++ train_skel.py
@@ -0,0 +1,30 @@
+
+
+def main():
+    # Init model
+    # Init solver
+    # Init gym
+    # Init adv table
+
+
+    training_iters = 1000
+    # Set inital state
+    for i in range(training_iters):
+        # update model
+        # solve model
+        # Build policy
+        # step gym env
+        # save in replay buffer (with solver gradient?)
+        # Sample replay buffer # (train every something i think)
+        # Extract adv
+        # update adv
+        # update weights
+        pass
+        
+
+
+
+
+
+if __name__ == "__main__":
+    main()
